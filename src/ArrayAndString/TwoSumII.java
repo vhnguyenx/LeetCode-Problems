1a8aa0bfd4be484bd7260e39e0704ab12f7c631f
Hoang Nguyen <vhnguyenx@gmail.com>
2025-10-24 00:43:05 +0700

    [167] Two Sum II - Input Array Is Sorted
    Using Two Pointer

---
+++ src/ArrayAndString/TwoSumII.java
@@ -0,0 +1,19 @@
+package ArrayAndString;
+
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {    
+        int left = 0;       
+        int right = numbers.length - 1;      
+        while (left < right) {      
+            int sum = numbers[left] + numbers[right];      
+            if (sum == target) {       
+                return new int[] { left + 1, right + 1 };       
+            } else if (sum < target) {       
+                left++;        
+            } else {        
+                right--;         
+            }
+        }      
+        return new int[] {};
+    }
+}
