b8f60ca1f427d7e3df81d95e16c8f0de69a9c8e3
Hoang Nguyen <vhnguyenx@gmail.com>
2025-10-24 03:21:53 +0700

    [27] Remove Element

---
+++ src/ArrayAndString/RemoveElement.java
@@ -0,0 +1,19 @@
+package ArrayAndString;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int leftPointer = 0;
+        int rightPointer = nums.length - 1;
+        while (leftPointer <= rightPointer) {
+            if (nums[leftPointer] == val) {
+                nums[leftPointer] = nums[rightPointer];
+                rightPointer--;
+            }
+            else
+            {
+                leftPointer++;
+            }
+        }
+        return leftPointer;
+    }
+}
