e65592c7bbd4fd9d04d795cdfd5ef4f3c1d68ae8
Hoang Nguyen <vhnguyenx@gmail.com>
2025-10-24 01:51:24 +0700

    [26] Remove Duplicates from Sorted Array

---
+++ src/ArrayAndString/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,14 @@
+package ArrayAndString;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int slow = 0;
+        for (int fast = 1; fast < nums.length; fast++) {
+            if (nums[slow] != nums[fast]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+        return slow + 1;
+    }
+}
