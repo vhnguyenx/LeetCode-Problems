f43ecd64e25ddab1446700bd24f5fc504819e152
Hoang Nguyen <vhnguyenx@gmail.com>
2025-10-27 21:54:41 +0700

    [283] Move Zeroes

---
+++ src/ArrayAndString/MoveZeroes.java
@@ -0,0 +1,15 @@
+package ArrayAndString;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                int temp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = temp;
+                slow++;
+            }
+        }
+    }
+}
