4175f2d9a17abf701848b34feb9385ba8b0f3c12
Hoang Nguyen <vhnguyenx@gmail.com>
2025-11-13 09:56:39 +0700

    [448] Find All Numbers Disappeared In An Array

---
+++ src/ArrayAndString/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,22 @@
+package ArrayAndString;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FindAllNumbersDisappearedInAnArray {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
