1c52424996fc161e9edfc5d003cf5d452c8fab34
Hoang Nguyen <vhnguyenx@gmail.com>
2025-11-19 20:58:52 +0700

    [485] Max Consecutive Ones

---
+++ src/Array/MaxConsecutiveOnes.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+                if (count > maxCount) {
+                    maxCount = count;
+
+                }
+            } else {
+
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+}
