4593617bba7549d8195fb119ca9935e9f0564816
Hoang Nguyen <vhnguyenx@gmail.com>
2025-11-20 14:12:29 +0700

    [136] Single Number

---
+++ src/Array/SingleNumber.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+       
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+      
+        return result;
+    }
+}
