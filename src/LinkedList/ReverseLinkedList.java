3ac28e955994a6e5687656ebe601b769a54a1ffe
Hoang Nguyen <vhnguyenx@gmail.com>
2025-11-26 22:31:11 +0700

    [206] Reverse Linked List

---
+++ src/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+public class ReverseLinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+
+        }
+
+        return prev;
+    }
+}
