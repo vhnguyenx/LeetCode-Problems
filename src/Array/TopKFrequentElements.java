6476e845f2648faf9f959cf120a9eb3af5f9dbfe
Hoang Nguyen <vhnguyenx@gmail.com>
2025-12-15 20:05:01 +0700

    [347] Top K Frequent Elements

---
+++ src/Array/TopKFrequentElements.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TopKFrequentElements {
+    // O (n log n)
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        int[] result = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            result[i] = list.get(i).getKey();
+        }
+
+        return result;
+    }
+}
