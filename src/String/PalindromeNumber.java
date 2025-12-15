885062ab8401ea528bda8070ab33a57fd6ce0ff3
Hoang Nguyen <vhnguyenx@gmail.com>
2025-12-15 20:26:26 +0700

    [9] Palindrome Number

---
+++ src/String/PalindromeNumber.java
@@ -0,0 +1,22 @@
+package String;
+
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        
+        String num = String.valueOf(x);
+
+        int left = 0;
+        int right = num.length() - 1;
+
+        while(left <= right){
+            if(num.charAt(left) != num.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+
+    }
+}
