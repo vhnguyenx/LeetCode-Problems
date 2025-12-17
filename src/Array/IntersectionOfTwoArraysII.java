d6805392d3af9457e42b5f3f110e3559021afb6e
vhnguyenx <vhnguyenx@gmail.com>
2025-12-17 20:53:05 +0700

    [350] Intersection of Two Arrays II

---
+++ src/Array/IntersectionOfTwoArraysII.java
@@ -0,0 +1,32 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class IntersectionOfTwoArraysII {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (Integer num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        for (Integer num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                list.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        int[] result = new int[list.size()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+}
