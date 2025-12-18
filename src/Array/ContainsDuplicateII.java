487f66a9b7054ab9f46ab9ff7b49fba3c6e34665
vhnguyenx <vhnguyenx@gmail.com>
2025-12-19 00:22:41 +0700

    [219] Contains Duplicate II

---
+++ src/Array/ContainsDuplicateII.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.HashMap;
+
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if ((Math.abs(i - map.get(nums[i]))) <= k) {
+                    return true;
+                }
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
