51e9c6b9f839cc868d87148e09b6414f54e78aa4
vhnguyenx <vhnguyenx@gmail.com>
2025-12-20 21:00:20 +0700

    [944] Delete Columns to Make Sorted

---
+++ src/Array/DeleteColumnsToMakeSorted.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class DeleteColumnsToMakeSorted {
+    public int minDeletionSize(String[] strs) {
+        int rows = strs.length;
+        int cols = strs[0].length();
+        int count = 0;
+
+        for (int col = 0; col < cols; col++) {
+            for (int row = 1; row < rows; row++) {
+                if (strs[row].charAt(col) < strs[row - 1].charAt(col)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+}
