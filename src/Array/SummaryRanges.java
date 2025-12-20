5126bf4adfb05dee5869d62a81f16a111804df07
vhnguyenx <vhnguyenx@gmail.com>
2025-12-20 22:21:18 +0700

    [228] Summary Ranges

---
+++ src/Array/SummaryRanges.java
@@ -0,0 +1,32 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SummaryRanges {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+
+        int i = 0;
+        while (i < nums.length) {
+
+            int start = nums[i];
+
+            while (i + 1 < nums.length && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+
+            int end = nums[i];
+
+            if (start == end) {
+                result.add(String.valueOf(start));
+            } else {
+                result.add(start + "->" + end);
+            }
+
+            i++;
+        }
+
+        return result;
+    }
+}
