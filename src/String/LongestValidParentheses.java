5f0b0578e544bb991940476c74026a6726a2a716
vhnguyenx <vhnguyenx@gmail.com>
2025-12-23 21:04:21 +0700

    [32] Longest Valid Parentheses

---
+++ src/String/LongestValidParentheses.java
@@ -0,0 +1,26 @@
+package String;
+
+import java.util.Stack;
+
+public class LongestValidParentheses {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int maxLen = 0;
+        for(int i = 0; i < s.length() ; i++){
+            if(s.charAt(i) == '('){
+                stack.push(i);
+            }else{
+                stack.pop();
+
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }else{
+                    maxLen = Math.max(maxLen, i - stack.peek());
+                }
+            }
+        }
+
+       
+    }
+}
