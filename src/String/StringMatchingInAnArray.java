1d78da0cc77408d2f971b0a3b229fe967e02f56b
vhnguyenx <vhnguyenx@gmail.com>
2026-01-01 22:45:25 +0700

    [1408] String Matching in an Array

---
+++ src/String/StringMatchingInAnArray.java
@@ -0,0 +1,22 @@
+package String;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringMatchingInAnArray {
+    public List<String> stringMatching(String[] words) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words.length; j++) {
+                if (words[i].contains(words[j]) && words[i] != words[j]) {
+                    if (!result.contains(words[j])) {
+                        result.add(words[j]);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
