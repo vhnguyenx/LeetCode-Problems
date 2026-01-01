bd82cf2b397b4b97e4255f3a1bbf0a9b47765913
vhnguyenx <vhnguyenx@gmail.com>
2026-01-01 21:22:08 +0700

    [422] Valid Word Square

---
+++ src/Array/ValidWordSquare.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.List;
+
+public class ValidWordSquare {
+    public boolean validWordSquare(List<String> words) {
+
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.get(i).length(); j++) {
+
+                if (j >= words.size()) {
+                    return false;
+                }
+
+                if (i >= words.get(j).length()) {
+                    return false;
+                }
+
+                if (words.get(i).charAt(j) != words.get(j).charAt(i)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
