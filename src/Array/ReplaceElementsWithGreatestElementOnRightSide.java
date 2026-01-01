fbe6a43c71f794c64a5ce3797a7cd7aff0be9746
vhnguyenx <vhnguyenx@gmail.com>
2026-01-01 19:06:56 +0700

    [1299] Replace Elements with Greatest Element on Right Side

---
+++ src/Array/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class ReplaceElementsWithGreatestElementOnRightSide {
+    public int[] replaceElements(int[] arr) {
+
+        int maxRight = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int current = arr[i];
+            arr[i] = maxRight;
+            maxRight = Math.max(maxRight, current);
+        }
+
+        return arr;
+    }
+}
