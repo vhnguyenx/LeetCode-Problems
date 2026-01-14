b38fd26abb754c49ded6084d015075210d9f172f
vhnguyenx <vhnguyenx@gmail.com>
2026-01-14 20:21:42 +0700

    [1752] Check if Array Is Sorted and Rotated

---
+++ src/Array/CheckIfArrayIsSortedAndRotated.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class CheckIfArrayIsSortedAndRotated {
+    public boolean check(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[(i + 1) % nums.length]) {
+                count++;
+            }
+
+            if (count > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
