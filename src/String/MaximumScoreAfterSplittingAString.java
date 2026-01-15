aa5b95832d3c3dc71a8455c3980794b174dd9481
vhnguyenx <vhnguyenx@gmail.com>
2026-01-15 19:42:44 +0700

    [1422] Maximum Score After Splitting a String

---
+++ src/String/MaximumScoreAfterSplittingAString.java
@@ -0,0 +1,27 @@
+package String;
+
+public class MaximumScoreAfterSplittingAString {
+    public int maxScore(String s) {
+        int ones = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                ones++;
+            }
+        }
+
+        int ans = 0;
+        int zeros = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '1') {
+                ones--;
+            } else {
+                zeros++;
+            }
+            ans = Math.max(ans, ones + zeros);
+        }
+
+        return ans;
+    }
+}
