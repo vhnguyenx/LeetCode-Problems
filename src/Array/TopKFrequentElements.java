6ad32a118447e26193b0d4aa22d2eaef16f4e6f4
vhnguyenx <vhnguyenx@gmail.com>
2026-01-25 21:06:03 +0700

    [347] Top K Frequent Elements (Bucket Sort - Optimize Solution)

--- src/Array/TopKFrequentElements.java
+++ src/Array/TopKFrequentElements.java
@@ -25,4 +25,35 @@ public class TopKFrequentElements {
 
         return result;
     }
+
+    public int[] topKFrequentV2(int[] nums, int k) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+
+        for (int key : freqMap.keySet()) {
+            int freq = freqMap.get(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(key);
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+     
+        for (int i = buckets.length - 1; i >= 1 && index < k; i--) {
+            if (buckets[i] != null) {
+                for (int val : buckets[i]) {
+                    result[index++] = val;
+                }
+            }
+        }
+
+        return result;
+    }
 }
