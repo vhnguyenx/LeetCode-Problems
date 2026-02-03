2dfab89bed442a09be1cdd7035c5fac12218f978
vhnguyenx <vhnguyenx@gmail.com>
2026-02-04 00:20:53 +0700

    [179] Largest Number

---
+++ src/Array/LargestNumber.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.util.Arrays;
+
+public class LargestNumber {
+    public String largestNumber(int[] nums) {
+        String[] arr = new String[nums.length];
+    
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        if (arr[0].equals("0")) {
+            return "0";
+        }
+       
+        StringBuffer str = new StringBuffer();
+       
+        for (int i = 0; i < arr.length; i++) {
+            str.append(arr[i]);
+        }
+
+        return str.toString();
+    }
+}
