0ec2b3280ee23d3509ca86a0e347ebacf64932f0
vhnguyenx <vhnguyenx@gmail.com>
2026-02-04 23:22:05 +0700

    [665] Non-decreasing Array

---
+++ src/Array/NonDecreasingArray.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class NonDecreasingArray {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1]) {
+                count++;
+
+                if (count > 1) {
+                    return false;
+                }
+
+                if (i - 2 < 0 || nums[i] >= nums[i - 2]) {
+                    nums[i - 1] = nums[i];
+                } else {
+                    nums[i] = nums[i - 1];
+                }
+            }
+        }
+
+        return true;
+    }
+}
