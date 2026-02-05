0ea632f8159a7f1311cb8cba9311143f82d77850
vhnguyenx <vhnguyenx@gmail.com>
2026-02-05 21:53:32 +0700

    [3379] Transformed Array

---
+++ src/Array/TransformedArray.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class TransformedArray {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int newIndex = ((i + nums[i]) % n + n) % n;
+            result[i] = nums[newIndex];
+        }
+
+        return result;
+    }
+}
