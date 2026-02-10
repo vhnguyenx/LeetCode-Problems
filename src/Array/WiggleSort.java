aaa6326ae4e340ad1f41c46939d67b3a746bb7b4
vhnguyenx <vhnguyenx@gmail.com>
2026-02-10 20:21:20 +0700

    [280] Wiggle Sort

---
+++ src/Array/WiggleSort.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class WiggleSort {
+    public void wiggleSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if (i % 2 != 0 && nums[i] < nums[i - 1]
+                    || i % 2 == 0 && nums[i] > nums[i - 1]) {
+
+                int temp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = temp;
+            }
+        }
+    }
+}
