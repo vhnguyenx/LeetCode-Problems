3a363c633f7303dd1c1316b6a97a5e0abd6d7565
vhnguyenx <vhnguyenx@gmail.com>
2026-02-17 11:15:37 +0700

    [905] Sort Array By Parity

---
+++ src/Array/SortArrayByParity.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class SortArrayByParity {
+      public int[] sortArrayByParity(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l < r) {
+            while (l < r && nums[l] % 2 == 0) {
+                l++;
+            }
+
+            while (l < r && nums[r] % 2 != 0) {
+                r--;
+            }
+
+            if (l < r) {
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+            }
+        }
+
+        return nums;
+    }
+}
